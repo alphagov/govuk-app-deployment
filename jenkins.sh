b11e44e225cd43228767c5d06cbdb5ba5513bf21
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-19 17:40:52 +0100

    Make alphagov-deployment files available to app's deployment scripts
    Change jenkins.sh to clone alphagov-deployment and copy the files to
    upload into a `secrets` subdirectory of the app's directory in this repo.
    
    We will also need to make the shared file upload steps look in the
    `secrets` directory, but we think that putting the files into a separate
    directory is cleaner than putting them directly into the app's
    directory, which runs the risk of overwriting existing files.
    
    Mobbed by @alexmuller @brenetic @emmabeynon @klssmith @jennyd

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+cd "$WORKSPACE"
+logger -p INFO -t jenkins "DEPLOYMENT: ${JOB_NAME} ${BUILD_NUMBER} ${TARGET_APPLICATION} ${TAG} (${BUILD_URL})"
+git clone git@github.gds:gds/alphagov-deployment.git
+cd $TARGET_APPLICATION
+if [ -d "../alphagov-deployment/${TARGET_APPLICATION}" ]; then
+  mkdir secrets
+  cp -r ../alphagov-deployment/$TARGET_APPLICATION/* secrets
+fi
