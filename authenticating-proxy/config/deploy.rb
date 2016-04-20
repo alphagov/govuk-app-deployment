5130c8fe47fd12bef978d7e67745df0eff041311
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-20 14:41:27 +0100

    Add authenticating-proxy config files

---
+++ authenticating-proxy/config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "authenticating-proxy"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, [
+  "draft_cache",
+]
+
+load 'defaults'
+load 'ruby'
+
+set :rails_env, 'production'
+
+namespace :deploy do
+  task :cold do
+    puts "There's no cold task for this project, just deploy normally"
+  end
+end
+
+after "deploy:notify", "deploy:notify:errbit"
