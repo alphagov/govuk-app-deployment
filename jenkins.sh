f698f3a68e1a7d9b17e04a9a47e81a94cc25ebda
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-20 11:46:50 +0100

    Add deployment configuration to jenkins.sh
    @brenetic @klssmith @emmabeynon @alexmuller

--- jenkins.sh
+++ jenkins.sh
@@ -8,3 +8,11 @@ if [ -d "../alphagov-deployment/${TARGET_APPLICATION}" ]; then
   mkdir secrets
   cp -r ../alphagov-deployment/$TARGET_APPLICATION/* secrets
 fi
+
+if [ -e "deploy.sh" ]; then
+  echo "---> Found deploy.sh, running 'sh -e deploy.sh'" >&2
+  exec sh -e deploy.sh
+else
+  echo "---> No deploy.sh found, running 'bundle exec cap \"${DEPLOY_TASK}\"'" >&2
+  exec bundle exec cap "$DEPLOY_TASK"
+fi
