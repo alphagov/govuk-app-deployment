550e4fe39bdd84b232c94641e6da3a02fd3c6268
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-22 15:18:51 +0100

    Add contacts-frontend config

---
+++ contacts-frontend/config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "contacts-frontend"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, [
+  "draft_frontend",
+  "frontend",
+]
+
+
+load 'defaults'
+load 'ruby'
+
+load 'deploy/assets'
+set :assets_prefix, 'contacts-frontend'
+
+set :source_db_config_file, false
+set :db_config_file, false
+
+set :config_files_to_upload, {
+  "secrets/to_upload/secrets.yml" => "config/secrets.yml",
+}
+
+after "deploy:notify", "deploy:notify:errbit"
