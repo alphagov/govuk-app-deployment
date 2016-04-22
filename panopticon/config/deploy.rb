dcfc1efebc310deda4562fabbfc522021b3ee6c8
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-22 17:53:48 +0100

    Add panopticon config

---
+++ panopticon/config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "panopticon"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, "backend"
+
+set :run_migrations_by_default, true
+
+load 'defaults'
+load 'ruby'
+load 'deploy/assets'
+
+load 'govuk_admin_template'
+
+set :rails_env, 'production'
+
+set :copy_exclude, [
+  'public/images',
+  'public/javascripts',
+  'public/stylesheets',
+  'public/templates'
+]
+
+# Include the Whenever cap task, and copy schedule.rb to production
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
+
+after "deploy:migrate", "deploy:create_mongoid_indexes"
+after "deploy:symlink", "deploy:seed_db"
+after "deploy:notify", "deploy:notify:errbit"
+after "deploy:restart", "deploy:restart_procfile_worker"
