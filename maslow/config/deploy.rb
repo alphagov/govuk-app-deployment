b3309ad819412ecec75745ff306ea2a8d4d879f2
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-22 17:47:09 +0100

    Add maslow config

---
+++ maslow/config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "maslow"
+
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, "backend"
+
+load 'defaults'
+load 'ruby'
+load 'deploy/assets'
+
+load 'govuk_admin_template'
+
+set :rails_env, 'production'
+
+set :source_db_config_file, 'secrets/to_upload/mongoid.yml'
+set :db_config_file, 'config/mongoid.yml'
+
+after "deploy:notify", "deploy:notify:errbit"
