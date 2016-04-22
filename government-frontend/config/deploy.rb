4acde122ee7f6c71b99477ca8b15daa3b46d272a
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2016-04-22 17:30:39 +0100

    Add government-frontend config

---
+++ government-frontend/config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "government-frontend"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, [
+  "draft_frontend",
+  "frontend",
+]
+
+load 'defaults'
+load 'ruby'
+load 'deploy/assets'
+
+set :assets_prefix, 'government-frontend'
+set :rails_env, 'production'
+
+after "deploy:notify", "deploy:notify:errbit"
