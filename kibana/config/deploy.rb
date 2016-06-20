9fcccbd264423c5331b38159c2d0e6da11e84c2e
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-06-20 18:24:54 +0100

    Add Kibana
    This is a simple app with no secrets.

---
+++ kibana/config/deploy.rb
@@ -0,0 +1,7 @@
+set :application, 'kibana'
+set :capfile_dir, File.expand_path("../", File.dirname(__FILE__))
+set :server_class, 'backend'
+set :repo_name, 'kibana-gds'
+
+load 'defaults'
+load 'ruby'
