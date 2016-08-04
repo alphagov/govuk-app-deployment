593615efa72d755b7f7c8c54b148620a9f10870e
Andrew Garner <andrew@andrewgarner.com>
2016-08-04 09:49:41 +0100

    Add recipe for Sidekiq Monitoring
    This is intended to replace the existing deployment recipe. The existing recipe
    needs updating to remove the syncing of Redis YAML config files. I've used
    that as an oppertunity to move to the new world.

---
+++ sidekiq-monitoring/config/deploy.rb
@@ -0,0 +1,9 @@
+set :application, 'sidekiq-monitoring'
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, 'backend'
+
+load 'defaults'
+load 'ruby'
+
+after 'deploy:restart', 'deploy:restart_procfile_worker'
+after 'deploy:notify', 'deploy:notify:errbit'
