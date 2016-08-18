1493ed695d9b6b7a8c9b8367afde13d0ea8f08fa
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-08-18 14:25:03 +0000

    Remove dfid-transition deploy scripts

--- dfid-transition/config/deploy.rb
+++
@@ -1,8 +0,0 @@
-set :application, "dfid-transition"
-set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
-set :server_class, "backend"
-
-load 'defaults'
-load 'ruby'
-
-after "deploy:restart", "deploy:restart_procfile_worker"
