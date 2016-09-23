ead9e4ab2a8a70cd42906f923e1e92535798007b
Seraphiana <seraphiana@hotmail.co.uk>
2016-09-23 11:45:14 +0100

    add files to redeploy specialist publisher rebuild in the place of  old specialist publisher

---
+++ specialist-publisher/config/deploy.rb
@@ -0,0 +1,12 @@
+set :application, "specialist-publisher"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, "backend"
+set :repo_name, "specialist-publisher-rebuild"
+
+load 'defaults'
+load 'ruby'
+load 'deploy/assets'
+load 'govuk_admin_template'
+
+after "deploy:restart", "deploy:restart_procfile_worker"
+after "deploy:notify", "deploy:notify:errbit"
