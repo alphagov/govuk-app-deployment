e9c31bd88818ecd632ba6fd201edd677753f5836
Mo Baig <mo@mobaig.com>
2016-09-28 16:44:26 +0100

    Correct rummager task names
    These apps are invoking the wrong task name.

--- businesssupportfinder/config/deploy.rb
+++ businesssupportfinder/config/deploy.rb
@@ -13,6 +13,6 @@ set :source_db_config_file, false
 set :db_config_file, false
 
 after "deploy:symlink", "deploy:panopticon:register"
-after "deploy:symlink", "deploy:rummager:index_all"
+after "deploy:symlink", "deploy:rummager:index"
 after "deploy:symlink", "deploy:publishing_api:publish"
 after "deploy:notify", "deploy:notify:errbit"
--- calendars/config/deploy.rb
+++ calendars/config/deploy.rb
@@ -19,6 +19,6 @@ end
 
 after "deploy:upload_initializers", "deploy:symlink_mailer_config"
 after "deploy:symlink", "deploy:panopticon:register"
-after "deploy:symlink", "deploy:rummager:index_all"
+after "deploy:symlink", "deploy:rummager:index"
 after "deploy:symlink", "deploy:publishing_api:publish"
 after "deploy:notify", "deploy:notify:errbit"
