8de821d4c83efb70d9ab2358f1556d84a5bb0436
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-09-29 14:39:41 +0000

    Remove upload config callback
    We don't want to install initializers from alphagov-deployment
    so remove the relevant after deploy callbacks.

--- specialist-publisher/config/deploy.rb
+++ specialist-publisher/config/deploy.rb
@@ -8,5 +8,15 @@ load 'ruby'
 load 'deploy/assets'
 load 'govuk_admin_template'
 
+
+# FIXME This is a temporary measure to prevent initializers from the original
+# Specialist Publisher application being deployed into the Specialist Publisher
+# Rebuild application. This can be safely removed once the initializers have
+# been removed from the old deployment repo.
+["deploy:upload_config", "deploy:upload_initializers"].each do |callback_source|
+  callback = callbacks[:after].find { |c| c.source == callback_source }
+  callbacks[:after].delete(callback)
+end
+
 after "deploy:restart", "deploy:restart_procfile_worker"
 after "deploy:notify", "deploy:notify:errbit"
