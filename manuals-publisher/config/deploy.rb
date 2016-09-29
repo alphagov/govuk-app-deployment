fccfcdd27687f939b45ef25eb8d1c1e067edef97
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-09-29 10:18:59 +0000

    Run migrations by default when deploying Manuals Publisher

--- manuals-publisher/config/deploy.rb
+++ manuals-publisher/config/deploy.rb
@@ -1,6 +1,7 @@
 set :application, "manuals-publisher"
 set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
 set :server_class, "backend"
+set :run_migrations_by_default, true
 
 load 'defaults'
 load 'ruby'
