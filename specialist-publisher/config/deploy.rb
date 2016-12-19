fd363548a4113ed222042720d41e5509a09e634d
Tijmen Brommet <tijmen@gmail.com>
2016-12-19 14:53:28 +0000

    Revert "Publish finders when specialist-publisher is deployed"

--- specialist-publisher/config/deploy.rb
+++ specialist-publisher/config/deploy.rb
@@ -7,13 +7,5 @@ load 'ruby'
 load 'deploy/assets'
 load 'govuk_admin_template'
 
-namespace :deploy do
-  desc "Publish all Finders to the Publishing API"
-  task :publish_finders do
-    run "cd #{current_release}; #{rake} publishing_api:publish_finders"
-  end
-end
-
-after "deploy:setup", "deploy:publish_finders"
 after "deploy:restart", "deploy:restart_procfile_worker"
 after "deploy:notify", "deploy:notify:errbit"
