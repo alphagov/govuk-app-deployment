32ca063f03dae2a7f29dfd625806bd5511984fb6
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-01-13 15:36:07 +0000

    Add a deploy script for content-performance-manager

---
+++ content-performance-manager/config/deploy.rb
@@ -0,0 +1,11 @@
+set :application, "content-performance-manager"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, "backend"
+
+set :run_migrations_by_default, true
+
+load 'defaults'
+load 'ruby'
+
+require "whenever/capistrano"
+set :whenever_command, "bundle exec whenever"
