fae1132be2e7a7f7cdf3985c12e9409811474ce3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-05-02 10:46:46 +0100

    Run hard_restart command from restart
    If we run hard_restart we have the problem that deploy:restart after
    hooks don't get called.
    
    It feels like it's easier to fix this problem with a "deploy:restart"
    that gets called whatever restart than it is to set up
    "deploy:hard_restart" since "deploy:restart" is such a common hook
    point.

--- sidekiq-monitoring/config/deploy.rb
+++ sidekiq-monitoring/config/deploy.rb
@@ -1,12 +1,8 @@
 set :application, 'sidekiq-monitoring'
 set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
 set :server_class, 'backend'
+set :perform_hard_restart, true
 
 load 'defaults'
 load 'ruby'
 
-namespace :deploy do
-  task :restart do
-    hard_restart
-  end
-end
