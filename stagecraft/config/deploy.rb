99a73fcdfc9df93ad6147f505c0c0ea04a001b0c
ambrozic <ambrozic@gmail.com>
2017-05-04 10:58:19 +0100

    Add tasks migrate, celerybeat and celerycam
    Changed migrate task to consider run_migrations flag, added missing tasks to restart celerybeat and celerycam.

--- stagecraft/config/deploy.rb
+++ stagecraft/config/deploy.rb
@@ -10,7 +10,9 @@ load "python"
 
 namespace :deploy do
   task :migrate, :only => { :primary => true } do
-    run_django_command("migrate --noinput")
+    if fetch(:run_migrations_by_default, true)
+      run_django_command("migrate --noinput")
+    end
   end
 
   task :static do
@@ -19,7 +21,7 @@ namespace :deploy do
 
   task :upload_environment_settings do
     config_folder = File.expand_path("settings/#{ENV['ORGANISATION']}", Dir.pwd)
-    if File.exists?(config_folder)
+    if File.exist?(config_folder)
       Dir.glob(File.join(config_folder, "*.py")).each do |settings|
         top.upload(settings, File.join(release_path, "stagecraft/settings/#{File.basename(settings)}"))
       end
@@ -31,11 +33,19 @@ namespace :deploy do
     run "test -d #{audit_dir_path} || mkdir #{audit_dir_path}"
   end
 
-  #override recipe due to stagecraft-worker having different name format to
-  #others.
-  desc "Restart the procfile worker"
+  desc "Restart the Celery workers"
   task :restart_procfile_worker do
-     run "sudo initctl start stagecraft-worker-procfile-worker 2>/dev/null || sudo initctl restart stagecraft-worker-procfile-worker"
+    run "sudo initctl start stagecraft-worker-procfile-worker 2>/dev/null || sudo initctl restart stagecraft-worker-procfile-worker"
+  end
+
+  desc "Restart CeleryBeat"
+  task :restart_celery_beat do
+    run "sudo initctl start stagecraft-beat-procfile-worker 2>/dev/null || sudo initctl restart stagecraft-beat-procfile-worker"
+  end
+
+  desc "Restart CeleryCam"
+  task :restart_celery_cam do
+    run "sudo initctl start stagecraft-celerycam-procfile-worker 2>/dev/null || sudo initctl restart stagecraft-celerycam-procfile-worker"
   end
 
   def run_django_command(command)
@@ -46,4 +56,4 @@ end
 before "deploy:install_deps", "deploy:upgrade_pip"
 before "deploy:finalize_update", "deploy:upload_environment_settings", "deploy:migrate", "deploy:static"
 after "deploy:link_shared_children", "deploy:setup_shared_audit_directory"
-after "deploy:restart", "deploy:restart_procfile_worker"
+after "deploy:restart", "deploy:restart_procfile_worker", "deploy:restart_celery_beat", "deploy:restart_celery_cam"
