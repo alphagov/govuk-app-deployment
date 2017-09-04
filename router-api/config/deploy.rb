61436b56407a556140ab4c36ce2949620cd41649
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2017-09-04 14:53:01 +0000

    Remove upload files for router-api.

--- router-api/config/deploy.rb
+++ router-api/config/deploy.rb
@@ -12,10 +12,5 @@ set :run_migrations_by_default, true
 load 'defaults'
 load 'ruby'
 
-set :config_files_to_upload, {
-  'secrets/to_upload/seeds/external_redirects.rb' => 'db/seeds/external_redirects.rb',
-}
-
 after "deploy:symlink", "deploy:create_mongoid_indexes"
-after "deploy:symlink", "deploy:seed_db"
 after "deploy:notify", "deploy:notify:error_tracker"
