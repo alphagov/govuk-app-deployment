d2b3799ad9749b9769c1ce267ffaa61a565b9ee4
Chris Lowis <chris.lowis@gmail.com>
2017-09-26 16:32:40 +0100

    Do not start or restart delayed job on asset manager deploy
    We have recently removed[1] delayed job from asset manager and
    migrated to sidekiq. This commit removes the start/restart task for
    asset-manager-delayed-job-worker so that the creation of this upstart
    service itself can also be removed from govuk-puppet.
    
    [1] https://github.com/alphagov/asset-manager/pull/232

--- asset-manager/config/deploy.rb
+++ asset-manager/config/deploy.rb
@@ -15,14 +15,8 @@ namespace :deploy do
   task :create_uploads_symlink do
     run "mkdir -p /data/uploads/asset-manager && ln -sfn /data/uploads/asset-manager #{latest_release}/uploads"
   end
-
-  desc "Restart the delayed_job worker"
-  task :restart_delayed_job do
-    run "sudo initctl start asset-manager-delayed-job-worker || sudo initctl restart asset-manager-delayed-job-worker"
-  end
 end
 
 after "deploy:update_code", "deploy:create_uploads_symlink"
-after "deploy:restart", "deploy:restart_delayed_job"
 after "deploy:restart", "deploy:restart_procfile_worker"
 after "deploy:notify", "deploy:notify:error_tracker"
