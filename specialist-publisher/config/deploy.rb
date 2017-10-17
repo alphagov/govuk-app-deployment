26dcbc646ca9ae9565d3e1db299929a86a54c0f0
brenetic <b@brenetic.com>
2017-10-17 10:26:03 +0000

    Publish Specialist Publisher special routes on deploy
    We need to publish special routes via a rake task on deploy.

--- specialist-publisher/config/deploy.rb
+++ specialist-publisher/config/deploy.rb
@@ -8,5 +8,6 @@ load 'deploy/assets'
 load 'govuk_admin_template'
 
 after "deploy:upload_initializers", "deploy:symlink_mailer_config"
+after "deploy:symlink", "deploy:publishing_api:publish_special_routes"
 
 after "deploy:restart", "deploy:restart_procfile_worker"
