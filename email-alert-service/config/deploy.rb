b142c4b7afbb55a39d6a35b70b64d896b23c3f2f
Ruben Arakelyan <ruben.arakelyan@digital.cabinet-office.gov.uk>
2018-02-21 13:55:44 +0000

    Deploy email-alert-api and email-alert-service to own machines
    This commit changes email-alert-api and email-alert-service to deploy to their own dedicated machines.

--- email-alert-service/config/deploy.rb
+++ email-alert-service/config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "email-alert-service"
 set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
-set :server_class, "backend"
+set :server_class, %w(email_alert_api backend)
 
 load 'defaults'
 load 'ruby'
