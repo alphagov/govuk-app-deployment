fd2320a2169333c7727c718f2b568a208078b6dd
David Silva <david.silva@digital.cabinet-office.gov.uk>
2018-05-29 11:50:45 +0100

    Add deploy scripts for organisations-publisher
    Trello: https://trello.com/c/WyEGklZ4/78-add-deployment-scripts

---
+++ organisations-publisher/config/deploy.rb
@@ -0,0 +1,13 @@
+set :application, "organisations-publisher"
+set :capfile_dir, File.expand_path('../', File.dirname(__FILE__))
+set :server_class, "backend"
+
+set :run_migrations_by_default, true
+
+load 'defaults'
+load 'ruby'
+load 'deploy/assets'
+
+set :source_db_config_file, false
+
+after "deploy:restart", "deploy:restart_procfile_worker"
