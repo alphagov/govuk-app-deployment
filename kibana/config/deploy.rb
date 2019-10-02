6f234f7dc9b0b35e62abefa1123d479fc8518b9e
Issy Long <me@issyl0.co.uk>
2019-10-02 14:47:10 +0100

    Remove errbit, govuk-cdn-logs-monitor, kibana and stagecraft
    - These apps are no longer running on GOV.UK, so no longer need
      deployment files here.
    - Errbit was replaced with hosted Sentry.
    - Kibana was replaced with hosted LogIt.
    - Stagecraft was moved to PaaS as part of the Performance Platform
      migration. (Backdrop is the only remaining Performance Platform app on
      GOV.UK, and is un-deployable.)
    - GOV.UK CDN Logs Monitor was retired based on the outcome of [RFC
      093](https://github.com/alphagov/govuk-rfcs/blob/master/rfc-093-retire-govuk-cdn-logs-monitor.md).

--- kibana/config/deploy.rb
+++
@@ -1,7 +0,0 @@
-set :application, "kibana"
-set :capfile_dir, File.expand_path("../", File.dirname(__FILE__))
-set :server_class, "backend"
-set :repo_name, "kibana-gds"
-
-load "defaults"
-load "ruby"
