8b833720c3b67dc912467db73156c85632900497
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2020-02-24 12:02:14 +0000

    Don't overwrite mailer config

--- support-api/config/deploy.rb
+++ support-api/config/deploy.rb
@@ -11,4 +11,3 @@ set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
 
 after "deploy:restart", "deploy:restart_procfile_worker"
-after "deploy:upload_initializers", "deploy:symlink_mailer_config"
