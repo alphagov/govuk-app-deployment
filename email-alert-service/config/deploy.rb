707ee073fedbf57df73369a341562504dfa6cf23
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-01-24 16:58:58 +0000

    Add update subscriber list queue consumer restart scripts
    [Trello](https://trello.com/c/IkYegsOt/1225-update-subscriberlist-titles-on-major-minor-publishing-change)
    
    Dependent on
    ============
    https://github.com/alphagov/email-alert-service/pull/493
    
    See also
    ========
    https://github.com/alphagov/govuk-app-deployment/pull/436/commits/5c4c0e28ac2132bcbafeaab6a75bbfa3a8a11c47

--- email-alert-service/config/deploy.rb
+++ email-alert-service/config/deploy.rb
@@ -17,7 +17,21 @@ namespace :deploy do
       run "sudo initctl restart email-alert-service-unpublishing-queue-consumer-procfile-worker || "\
           "sudo initctl start email-alert-service-unpublishing-queue-consumer-procfile-worker"
     end
+
+    desc "Restart the update subscriber list details minor consumer"
+    task :restart_subscriber_list_details_update_minor_consumer do
+      run "sudo initctl restart email-alert-service-subscriber-list-details-update-minor-consumer-procfile-worker || "\
+          "sudo initctl start email-alert-service-subscriber-list-details-update-minor-consumer-procfile-worker"
+    end
+
+    desc "Restart the update subscriber list details major consumer"
+    task :restart_subscriber_list_details_update_major_consumer do
+      run "sudo initctl restart email-alert-service-subscriber-list-details-update-major-consumer-procfile-worker || "\
+          "sudo initctl start email-alert-service-subscriber-list-details-update-major-consumer-procfile-worker"
+    end
   end
 end
 
 after "deploy:restart", "deploy:email_alert_service:restart_unpublishing_queue_consumer"
+after "deploy:restart", "deploy:email_alert_service:restart_subscriber_list_details_update_major_consumer"
+after "deploy:restart", "deploy:email_alert_service:restart_subscriber_list_details_update_minor_consumer"
